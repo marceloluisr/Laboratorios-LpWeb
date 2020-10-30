968cdc98374ea798ae1c81ab6c605e5ba441b539
marceloluisr <marceloluis.pessoal@gmail.com>
2020-10-30 16:54:27 -0300

    Adicionado caixa de pesquisa com JS p/ tela small

---
+++ Lab3 - Bootstrap/js/index.js
@@ -0,0 +1,6 @@
+function carregarSearchBar() {
+    document.getElementById("barra-pesquisa").innerHTML = 
+        '<div class="input-group" style="position:relative;right:1rem"><input placeholder="Pesquise no Book.net" type="text" class="form-control form-control-range"><button type="button" class="btn btn-success"><span style="color:white;">IR</button></span> </div>';
+
+       
+}
